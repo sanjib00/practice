3f72057cbb252b9a9b0708c6fa044ecd15961c1a
towfiqpiash <towfiqpiash@gmail.com>
2014-07-14 11:11:16 +0600

    Arithmetic calculation

---
+++ arithmetic.php
@@ -0,0 +1,38 @@
+<?php
+
+class Arithmetic{
+
+public $num1;
+public $num2;
+
+
+
+public function add_numbers()
+{
+
+return $this->num1+$this->num2;
+
+}
+public function subtract_numbers()
+{
+
+return $this->num1-$this->num2;
+
+}
+public function multiply_numbers()
+{
+
+return $this->num1*$this->num2;
+
+}
+public function divide_numbers()
+{
+
+return $this->num1/$this->num2;
+
+}
+
+}
+
+
+?>
---
+++ index.php
@@ -0,0 +1,66 @@
+<html>
+
+<body>
+
+<form action = "index.php" method = "GET">
+
+First Number:<input type = 'text' name = 'num1'> <br />
+Second Number:<input type = 'text' name = 'num2'> <br/>
+
+<input type = "submit" value = "Add" name = 'submitAdd'>
+<input type = "submit" value = "Subtract" name = 'submitSub'>
+<input type = "submit" value = "Multiply" name = 'submitMul'>
+<input type = "submit" value = "Divide" name = 'submitDiv'>
+
+<br />
+
+
+</form>	
+<?php
+require 'arithmetic.php';
+
+if(isset($_GET['submitAdd']))
+{
+$input = new Arithmetic();
+$input->num1 = $_GET['num1'];
+$input->num2 = $_GET['num2'];
+
+echo $input->add_numbers();
+
+}
+
+if(isset($_GET['submitSub']))
+{
+$input = new Arithmetic();
+$input->num1 = $_GET['num1'];
+$input->num2 = $_GET['num2'];
+echo $input->subtract_numbers();
+
+}
+
+if(isset($_GET['submitMul']))
+{
+$input = new Arithmetic();
+$input->num1 = $_GET['num1'];
+$input->num2 = $_GET['num2'];
+echo $input->multiply_numbers();
+
+}
+if(isset($_GET['submitDiv']))
+{
+$input = new Arithmetic();
+$input->num1 = $_GET['num1'];
+$input->num2 = $_GET['num2'];
+echo $input->divide_numbers();
+
+}
+
+
+
+
+
+
+?>
+
+</body>
+</html> 
