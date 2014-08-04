e803387d39a453e76a80b52a69593a0ce55c4b27
Sanjib <sanjib@hotmail.com>
2014-08-04 21:53:51 +0600

    use function

---
+++ function-2.php
@@ -0,0 +1,23 @@
+<?php
+
+	function name($lastname,$place)
+	{
+		echo "my lastname is $lastname.\n";
+
+		if($lastname == "Ali")
+		{	
+			
+			return;
+		}
+
+		echo " I'm from $place.<br/>";
+	}
+
+	name("Ali","Modonpur");
+	name("Sohag","Bogra");
+	name("Manik","Rajshahi");
+	name("Rashel","Pagla");
+	name("Piash","Feni");
+
+
+?>
