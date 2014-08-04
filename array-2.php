2ab397b7eab4527607e966eff344d6a78e614d07
Sanjib <sanjib@hotmail.com>
2014-08-04 21:48:18 +0600

    understaning array

---
+++ array-2.php
@@ -0,0 +1,13 @@
+<?php
+
+$fruits = array("Tomato","Potato","Mango","Lichi","Guava");
+$fruits[1] = "Graves";
+
+print_r($fruits);
+
+echo "I like ".$fruits[2]." very much.";
+
+$browser = array(0=>'mozilla',1=>'Chrome',3=>'Opera',4=>'Explorer');
+print_r($browser);
+
+?>
