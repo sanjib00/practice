f6d7a35f36777ea6daa45d2c3d21bdf5b4cff001
Sanjib <sanjib@hotmail.com>
2014-08-04 21:56:11 +0600

    variable validity

---
+++ variablevalidity.php
@@ -0,0 +1,15 @@
+<?php
+	
+	$name = 'Rahul'; //valid
+
+	$_name = 'Rajesh'; //Valid
+
+	$1name = 'Maruf'; //Invalid
+
+	echo($name)."<br/>";
+	echo($_name)."<br/>";
+	echo($1name)."<br/>";
+
+
+
+?>
