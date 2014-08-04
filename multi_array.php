fc2aa18148ee846d1c814893ecb82af42680f1b6
Sanjib <sanjib@hotmail.com>
2014-08-04 21:54:27 +0600

    understanding multi-dimensional array

---
+++ multi_array.php
@@ -0,0 +1,30 @@
+<?php
+
+	$families = array(
+
+		"Sanjib"=>array(
+
+			"Rasel"=>array(20,33,25,89,),
+			"Mitun"=>22,
+			"Nitish"=>21,
+		),
+
+		"Pagla"=>array(
+
+			"Mitu",
+			"Uha",
+			"Mobin"
+		),
+
+		"Bablu"=>array(
+
+			"Rahul",
+			"Nipa",
+			"Shuvo"
+		)
+
+	);
+
+	print_r($families['Sanjib']['Rasel'][2]);
+
+?>
