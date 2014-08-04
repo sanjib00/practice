970f2bb91f4de586e18331a7e79cbe7545fd997f
Sanjib <sanjib@hotmail.com>
2014-08-04 21:45:16 +0600

    use operator

---
+++ arithmetic_op.php
@@ -0,0 +1,27 @@
+<?php
+
+	/*$a = 58.22;
+	$b = 20.14;
+
+	echo ($a + $b)."\n";
+
+	echo ($a - $b)."\n";
+
+	echo ($a * $b)."\n";
+
+	echo ($a % $b)."\n";
+	*/
+
+	$a = 1;
+
+	echo $a++."<br/>"; //print 1, value 2
+
+	echo ++$a."<br/>";//print 3, value 3
+
+	echo --$a."<br/>";//print 2, value 2
+
+	echo $a--."<br/>";//print 2, value 1
+
+
+	
+?>
