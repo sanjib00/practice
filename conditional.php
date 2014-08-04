ca6292f5d02fb9dba5e8933c2dcb758a4c3b98e2
Sanjib <sanjib@hotmail.com>
2014-08-04 21:51:26 +0600

    use conditional operator

---
+++ conditional.php
@@ -0,0 +1,29 @@
+<?php
+
+	$x = 10.112;
+
+	if (10 == $x)
+	{
+		echo "yes";
+	}
+
+	else
+	{
+		echo "NO";
+	}
+
+	$i = 0;
+	while ($i<10) {
+
+		echo ($i)."\n";
+		$i++;
+	}
+
+	echo "<br/>";
+
+	for ($j=0; $j<=10; $j++)
+	{
+		echo($j).PHP_EOL;
+	}
+
+?>
